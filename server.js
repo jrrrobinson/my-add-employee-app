b396286b776321997be7d8df4ebd03fbdc15145b
Jacob Robinson <jrrobinson@myseneca.ca>
2017-12-04 10:11:40 -0500

    first commit

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 
 function onHttpStart() {
@@ -33,6 +37,27 @@ function onHttpStart() {
 
 // Load CSS file
 app.use(express.static('public'));
+
+//
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine(".hbs", exphbs({
+  extname: ".hbs",
+  defaultLayout: 'layout',
+  helpers: {
+    equal: function (lvalue, rvalue, options) {
+      if (arguments.length < 3)
+        throw new Error("Handlebars Helper equal needs 2 parameters");
+      if (lvalue != rvalue) {
+        return options.inverse(this);
+      } else {
+        return options.fn(this);
+      }
+    }
+  }
+}));
+app.set("view engine", ".hbs");
 
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res){
