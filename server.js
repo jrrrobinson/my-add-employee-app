66f2d8d3f1f05aebdb2f00995ebde6bd69369c27
Jacob Robinson <jrrobinson@myseneca.ca>
2017-12-04 23:03:01 -0500

    first commit

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ var express = require("express");
 var app = express();
 var path = require("path");
 var data_service = require("./data-service.js");
+
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 
-var HTTP_PORT = process.env.PORT || 8080;
+const HTTP_PORT = process.env.PORT || 8080;
 
 function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
+
     return new Promise((res, req) => {
         data_service.initialize().then((data) => {
             console.log(data)
@@ -21,30 +23,27 @@ function onHttpStart() {
 // Load CSS file
 app.use(express.static('public'));
 
+// ensures that the bodyParser middleware will work correctly
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.engine(".hbs", exphbs({
-    extname: ".hbs",
-    defaultLayout: 'layout',
-    helpers: {
-        equal: (lvalue, rvalue, options) => {
-            if (arguments.length < 3)
-                throw new Error("Handlebars Helper equal needs 2 parameters");
-            if (lvalue != rvalue) {
-                return options.inverse(this);
-            } else {
-                return options.fn(this);
-            }
-        }
+  extname: ".hbs",
+  defaultLayout: 'layout',
+  helpers: {
+    equal: function (lvalue, rvalue, options) {
+      if (arguments.length < 3)
+        throw new Error("Handlebars Helper equal needs 2 parameters");
+      if (lvalue != rvalue) {
+        return options.inverse(this);
+      } else {
+        return options.fn(this);
+      }
     }
+  }
 }));
 app.set("view engine", ".hbs");
 
-// alternative method.
-// app.use(express.static(path.join(__dirname, 'public')));
-
-/////////////////////////////////////Get Route //////////////////////////////////
-
-// setup a 'route' to listen on the default url path (http://localhost)
+// setup 'route' to listen on the default url path
 app.get("/", (req, res) => {
     res.render("home");
 });
@@ -83,34 +82,10 @@ app.get("/employees", (req, res) => {
 });
 
 app.get("/employee/:empNum", (req, res) => {
-    // initialize an empty object to store the values
-    let viewData = {};
     data_service.getEmployeeByNum(req.params.empNum).then((data) => {
-        viewData.data = data; //store employee data in the "viewData" object as "data"
-    }).catch(() => {
-        viewData.data = null; // set employee to null if there was an error
-    }).then(data_service.getDepartments).then((data) => {
-        viewData.departments = data; // store department data in the "viewData" object as "departments"
-                                     // loop through viewData.departments and once we have found the departmentId that matches
-                                     // the employee's "department" value, add a "selected" property to the matching
-                                     // viewData.departments object
-        for (let i = 0; i < viewData.departments.length; i++) {
-            if (viewData.departments[i].departmentId == viewData.data[0].department) {
-                viewData.departments[i].selected = true;
-            }
-        }
-        // if not add department set Selected to false and promto a message to user, message like "Please Choose Department" in html.
-        if (viewData.departments[viewData.departments.length-1].departmentId != viewData.data[0].department) {
-            viewData.departments.Selected = false;
-        }
-    }).catch(() => {
-        viewData.departments = []; // set departments to empty if there was an error
-    }).then(() => {
-        if (viewData.data == null){ // if no employee - return an error
-            res.status(404).send("Employee Not Found!!!");
-        } else {
-            res.render("employee", { viewData: viewData }); // render the "employee" view
-        }
+        res.render("employee", { data: data });
+    }).catch((err) => {
+        res.status(404).send("Employee Not Found!");
     });
 });
 
@@ -131,73 +106,34 @@ app.get("/departments", (req, res) => {
 });
 
 app.get("/employees/add", (req, res) => {
-    data_service.getDepartments().then((data) => {
-        res.render("addEmployee",{departments: data});
-    }).catch((err) => {
-        res.render("addEmployee", {departments: []});
-    });
+    res.render("addEmployee");
 });
-
-app.get("/departments/add", (req, res) => {
-    res.render("addDepartment",{title: "Department"});
-});
-
-app.get("/employee/delete/:empNum", (req, res) => {
-    data_service.deleteEmployeeByNum(req.params.empNum).then((data) => {
-        res.redirect("/employees");
-    }).catch((err) => {
-        res.status(500).send("Unable to Remove Employee / Employee not found");
-    });
-});
-
-app.get("/department/:departmentId", (req, res) => {
-    data_service.getDepartmentById(req.params.departmentId).then((data) => {
-        res.render("department", {
-           data: data
-        });
-    }).catch((err) => {
-        res.status(404).send("Department Not Found");
-    });
-});
-
-/////////////////////////////////////Post Route /////////////////////////////////
 
 app.post("/employees/add", (req, res) => {
     data_service.addEmployee(req.body).then((data) => {
+        console.log(req.body);
         res.redirect("/employees");
     }).catch((err) => {
         console.log(err);
-    });
+    })
 });
 
 app.post("/employees/update", (req, res) => {
+    console.log(req.body);
     res.redirect("/employees");
 });
 
 app.post("/employee/update", (req, res) => {
     data_service.updateEmployee(req.body).then((data) => {
+        console.log(req.body);
         res.redirect("/employees");
     }).catch((err) => {
         console.log(err);
-    });
-});
-
-app.post("/departments/add", (req, res) => {
-    data_service.addDepartment(req.body).then((data) => {
-        res.redirect("/departments");
-    }).catch(() => {
-        console.log(err);
-    });
-});
-
-app.post("/department/update", (req,res) => {
-    data_service.updateDepartment(req.body).then((data) => {
-        res.redirect("/departments");
-    });
+    })
 });
 
 app.use((req, res) => {
-    res.status(404).send("Sorry!!!!!!!>>>Page Not Found! <<<:(");
+    res.status(404).send("Sorry - Page not found");
 });
 
 app.listen(HTTP_PORT, onHttpStart);
